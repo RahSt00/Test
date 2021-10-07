d9080ba07c8eed1bc42b8dc1981451a52632dab7
RahSt00 <2024018@stud.hs-mannheim.de>
2021-10-07 20:31:40 +0200

    Eine kleine Funktion

--- TestProjekt/src/NiceTry.java
+++ TestProjekt/src/NiceTry.java
@@ -3,7 +3,11 @@ public class NiceTry {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+// Erstellen wir eine simple Funktion
+		
+		int a= 23;
+		int b= 43;
+		System.out.println(a+b);
 	}
 
 }
